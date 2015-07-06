e2f89b759e180d74c79f719ebac92c3db3878e92
rogithub <rosec@protonmail.ch>
2015-07-06 16:02:42 -0500

    added content to controller

--- web/public/angular/controllers.js
+++ web/public/angular/controllers.js
@@ -1,7 +1,14 @@
 (function() {
     var app = angular.module('app');
     
-    app.controller('StudentsController', ['$http', function($http){
-       this.name = 'rodrigo';
+    app.controller('StudentsController', ['$http', function($http) {
+        var model = this;
+        model.students = [];
+                
+        $http.get('/api/students').success(function(data){
+            model.students = data;
+            console.dir(data);
+        });
+        
     }]);
 })();
